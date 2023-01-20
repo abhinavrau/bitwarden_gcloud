13e5b49e6f0cb5ecd119765a8166b6d657ec415b
abhinavrau <abhinav.rau@gmail.com>
2023-01-20 11:38:54 -0500

    updates for exteral access

--- images/tailscale_caddy/start.sh
+++ images/tailscale_caddy/start.sh
@@ -4,9 +4,11 @@ echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
 tailscaled --tun=userspace-networking --statedir=${TAILSCALE_STATE_DIR} --state=${TAILSCALE_STATE_ARG} &
 PID=$!
+echo "---------Starting Tailscale network----------"
 until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
     sleep 0.1
 done
+echo "---------Starting Tailscale proxy----------"
 tailscale serve / proxy ${TS_PORT:-8080}
 
 echo "---------Starting Tailscale proxy----------"
